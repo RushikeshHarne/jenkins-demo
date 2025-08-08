88de54da6b98e7aaf93611246441ac223c7cb0c3
Rushikesh Harne <161678001+RushikeshHarne@users.noreply.github.com>
2025-08-08 19:50:07 +0530

    Update update_motd.sh

--- update_motd.sh
+++ update_motd.sh
@@ -3,3 +3,4 @@ echo '#!/bin/bash' | sudo tee /etc/update-motd.d/99-jenkins > /dev/null
 echo 'echo "hello from jenkins"' | sudo tee -a /etc/update-motd.d/99-jenkins > /dev/null
 sudo chmod +x /etc/update-motd.d/99-jenkins
 echo "MOTD updated successfully."
+cat /etc/motd
